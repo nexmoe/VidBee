737f869beb69f08b803ebf1448d5f8bbcfa37160
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2026-01-23 21:56:49 +0800

    fix(docs): wrap default layout provider

--- docs/src/app/(docs)/layout.tsx
+++ docs/src/app/(docs)/layout.tsx
@@ -3,6 +3,19 @@ import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
 import { i18n } from '@/lib/i18n';
+import { RootProvider } from 'fumadocs-ui/provider/next';
+import { defineI18nUI } from 'fumadocs-ui/i18n';
+
+const { provider } = defineI18nUI(i18n, {
+  translations: {
+    en: {
+      displayName: 'English',
+    },
+    zh: {
+      displayName: '中文',
+    },
+  },
+});
 
 export default async function Layout({
   children,
@@ -11,8 +24,17 @@ export default async function Layout({
 }) {
   const locale = i18n.defaultLanguage;
   return (
-    <DocsLayout tree={source.getPageTree(locale)} {...baseOptions(locale)}>
-      {children}
-    </DocsLayout>
+    <RootProvider
+      i18n={provider(locale)}
+      search={{
+        options: {
+          type: 'static',
+        },
+      }}
+    >
+      <DocsLayout tree={source.getPageTree(locale)} {...baseOptions(locale)}>
+        {children}
+      </DocsLayout>
+    </RootProvider>
   );
 }
